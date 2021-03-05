96fb82fb52e6474d8cb718d5ccde776f288f9819
EC2 Default User <ec2-user@ip-172-31-94-7.ec2.internal>
2021-03-05 23:32:29 +0000

    [Finish] 2-7

---
+++ add.rb
@@ -0,0 +1,7 @@
+for i in 1..6 do
+  puts i
+end
+
+# (1..6).each do |i|
+#   puts i
+# end
---
+++ break.rb
@@ -0,0 +1,19 @@
+i = 1
+while i <= 10 do
+  if i == 5
+    puts "処理を終了します"
+    break
+  end
+  puts i
+  i += 1
+end
+
+# i = 1
+# while i <= 10 do
+#   puts i
+#   i+= 1
+#   if i == 5
+#     puts "処理を終了します"
+#     break
+#   end
+# end
